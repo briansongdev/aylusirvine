dd874ef43aeafc27e766de9a1ee50b48176d184e
Brian Song <brians3476@gmail.com>
2020-08-30 14:16:15 -0700

    server

--- server.js
+++ server.js
@@ -31,12 +31,9 @@ app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
 
-if (
-  process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "staging"
-) {
-  app.use(express.static("src/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/src/build/index.html"));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
 }
