7595ac2d4a9b43ab91128310dda9710d4f790226
Brian Song <brians3476@gmail.com>
2020-08-31 15:50:15 -0700

    small issues, but UI is up

--- client/src/landing/App.js
+++ client/src/landing/App.js
@@ -35,13 +35,11 @@ function App() {
         localStorage.setItem("auth-token", "");
         token = "";
       }
-      const tokenRes = await Axios.post(
-        "http://localhost:5000/users/tokenIsValid",
-        null,
-        { headers: { "x-auth-token": token } }
-      );
+      const tokenRes = await Axios.post("/api/users/tokenIsValid", null, {
+        headers: { "x-auth-token": token },
+      });
       if (tokenRes.data) {
-        const userRes = await Axios.get("http://localhost:5000/users/", {
+        const userRes = await Axios.get("/api/users/", {
           headers: { "x-auth-token": token },
         });
         if (userRes.data.email == "brians3476@gmail.com") {
