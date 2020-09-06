8dff834a4ec81f741a5b46f216adfa3b1429a2bc
Brian Song <brians3476@gmail.com>
2020-09-05 21:38:22 -0700

    heroku error

--- client/src/landing/Navigation.js
+++ client/src/landing/Navigation.js
@@ -270,7 +270,7 @@ export default function Navigation() {
         ) : (
           <></>
         )}
-        <Link>
+        <List>
           <ListItem>
             <ListItemText secondary={"made with ❤️ by Brian."} />
           </ListItem>
