ff28361d30af0f7baa8d560645920a0cf9aaad22
Brian Song <brians3476@gmail.com>
2020-09-26 15:46:58 -0700

    amination final

--- client/src/components/events-list.component.js
+++ client/src/components/events-list.component.js
@@ -141,153 +141,155 @@ class EventList extends PureComponent {
   notSignedIn() {
     return (
       <Container className="p-3 text-center">
-        {isMobile ? (
-          <>
-            <Row className="justify-content-center">
-              <h6 style={{ color: " red" }}>
-                Best viewed in horizontal or on a computer.
-              </h6>
-            </Row>
-          </>
-        ) : (
-          <></>
-        )}
-        <h3>Welcome!</h3>
-        <img
-          src={require("../images/aylus-logo.png")}
-          class="rounded mx-auto d-block"
-          alt="..."
-        />
-        <Row className="p-4 justify-content-center">
-          <p style={{ lineHeight: "200%" }}>
-            <span style={{ fontWeight: "bold" }}>AYLUS Irvine</span> has an
-            initiative of giving back to the community through service in
-            multifaceted events for the betterment of society. We strive to
-            cultivate a love for volunteering.
-            <br />
-            <br />
-            <span style={{ fontWeight: "bold" }}>
-              This is where you can access our current events (and sign up for
-              them, check your hours, etc.).
-            </span>
-            <br />
-            <br />
-            The projects we do include{" "}
-            <span style={{ color: "#ff0000" }}>h</span>
-            <span style={{ color: "#fc0103" }}>e</span>
-            <span style={{ color: "#fa0205" }}>l</span>
-            <span style={{ color: "#f70308" }}>p</span>
-            <span style={{ color: "#f5040a" }}>i</span>
-            <span style={{ color: "#f2050d" }}>n</span>
-            <span style={{ color: "#ef0610" }}>g</span>
-            <span style={{ color: "#ed0712" }}> </span>
-            <span style={{ color: "#ea0815" }}>o</span>
-            <span style={{ color: "#e80917" }}>u</span>
-            <span style={{ color: "#e5091a" }}>t</span>
-            <span style={{ color: "#e20a1d" }}> </span>
-            <span style={{ color: "#e00b1f" }}>a</span>
-            <span style={{ color: "#dd0c22" }}>t</span>
-            <span style={{ color: "#db0d24" }}> </span>
-            <span style={{ color: "#d80e27" }}>s</span>
-            <span style={{ color: "#d50f2a" }}>e</span>
-            <span style={{ color: "#d3102c" }}>n</span>
-            <span style={{ color: "#d0112f" }}>i</span>
-            <span style={{ color: "#ce1231" }}>o</span>
-            <span style={{ color: "#cb1334" }}>r</span>
-            <span style={{ color: "#c81437" }}> </span>
-            <span style={{ color: "#c61539" }}>c</span>
-            <span style={{ color: "#c3163c" }}>e</span>
-            <span style={{ color: "#c1173e" }}>n</span>
-            <span style={{ color: "#be1841" }}>t</span>
-            <span style={{ color: "#bb1944" }}>e</span>
-            <span style={{ color: "#b91a46" }}>r</span>
-            <span style={{ color: "#b61b49" }}>s</span>
-            <span style={{ color: "#b41c4b" }}>,</span>
-            <span style={{ color: "#b11c4e" }}> </span>
-            <span style={{ color: "#ae1d51" }}>t</span>
-            <span style={{ color: "#ac1e53" }}>e</span>
-            <span style={{ color: "#a91f56" }}>a</span>
-            <span style={{ color: "#a72058" }}>c</span>
-            <span style={{ color: "#a4215b" }}>h</span>
-            <span style={{ color: "#a1225e" }}>i</span>
-            <span style={{ color: "#9f2360" }}>n</span>
-            <span style={{ color: "#9c2463" }}>g</span>
-            <span style={{ color: "#9a2565" }}> </span>
-            <span style={{ color: "#972668" }}>y</span>
-            <span style={{ color: "#94276b" }}>o</span>
-            <span style={{ color: "#92286d" }}>u</span>
-            <span style={{ color: "#8f2970" }}>n</span>
-            <span style={{ color: "#8d2a72" }}>g</span>
-            <span style={{ color: "#8a2b75" }}> </span>
-            <span style={{ color: "#872c78" }}>s</span>
-            <span style={{ color: "#852d7a" }}>t</span>
-            <span style={{ color: "#822e7d" }}>u</span>
-            <span style={{ color: "#7f2e80" }}>d</span>
-            <span style={{ color: "#7d2f82" }}>e</span>
-            <span style={{ color: "#7a3085" }}>n</span>
-            <span style={{ color: "#783187" }}>t</span>
-            <span style={{ color: "#75328a" }}>s</span>
-            <span style={{ color: "#72338d" }}>,</span>
-            <span style={{ color: "#70348f" }}> </span>
-            <span style={{ color: "#6d3592" }}>s</span>
-            <span style={{ color: "#6b3694" }}>e</span>
-            <span style={{ color: "#683797" }}>r</span>
-            <span style={{ color: "#65389a" }}>v</span>
-            <span style={{ color: "#63399c" }}>i</span>
-            <span style={{ color: "#603a9f" }}>n</span>
-            <span style={{ color: "#5e3ba1" }}>g</span>
-            <span style={{ color: "#5b3ca4" }}> </span>
-            <span style={{ color: "#583da7" }}>a</span>
-            <span style={{ color: "#563ea9" }}>t</span>
-            <span style={{ color: "#533fac" }}> </span>
-            <span style={{ color: "#5140ae" }}>l</span>
-            <span style={{ color: "#4e41b1" }}>o</span>
-            <span style={{ color: "#4b41b4" }}>c</span>
-            <span style={{ color: "#4942b6" }}>a</span>
-            <span style={{ color: "#4643b9" }}>l</span>
-            <span style={{ color: "#4444bb" }}> </span>
-            <span style={{ color: "#4145be" }}>f</span>
-            <span style={{ color: "#3e46c1" }}>o</span>
-            <span style={{ color: "#3c47c3" }}>o</span>
-            <span style={{ color: "#3948c6" }}>d</span>
-            <span style={{ color: "#3749c8" }}> </span>
-            <span style={{ color: "#344acb" }}>b</span>
-            <span style={{ color: "#314bce" }}>a</span>
-            <span style={{ color: "#2f4cd0" }}>n</span>
-            <span style={{ color: "#2c4dd3" }}>k</span>
-            <span style={{ color: "#2a4ed5" }}>s</span>
-            <span style={{ color: "#274fd8" }}>,</span>
-            <span style={{ color: "#2450db" }}> </span>
-            <span style={{ color: "#2251dd" }}>a</span>
-            <span style={{ color: "#1f52e0" }}>n</span>
-            <span style={{ color: "#1d53e2" }}>d</span>
-            <span style={{ color: "#1a54e5" }}> </span>
-            <span style={{ color: "#1754e8" }}>m</span>
-            <span style={{ color: "#1555ea" }}>u</span>
-            <span style={{ color: "#1256ed" }}>c</span>
-            <span style={{ color: "#1057ef" }}>h</span>
-            <span style={{ color: "#0d58f2" }}> </span>
-            <span style={{ color: "#0a59f5" }}>m</span>
-            <span style={{ color: "#085af7" }}>o</span>
-            <span style={{ color: "#055bfa" }}>r</span>
-            <span style={{ color: "#035cfc" }}>e</span>
-            <span style={{ color: "#005dff" }}>.</span> Our events are mainly
-            targeted at helping society while also allowing volunteers to gain
-            more team and leadership experience. <br />
-            <br /> We ARE active during COVID-19, and{" "}
-            <span style={{ fontWeight: "bold" }}>everyone is welcome!</span>
-          </p>
-        </Row>
-        <Row className="p-3 justify-content-center">
-          <h5>
-            To get started, <Link to="/register">register.</Link>
-          </h5>
-        </Row>
-        <Row className="p-1 justify-content-center">
-          <h6>
-            For more info, consult our <Link to="/faq">FAQ.</Link>
-          </h6>
-        </Row>
+        <FadeIn>
+          {isMobile ? (
+            <>
+              <Row className="justify-content-center">
+                <h6 style={{ color: " red" }}>
+                  Best viewed in horizontal or on a computer.
+                </h6>
+              </Row>
+            </>
+          ) : (
+            <></>
+          )}
+          <h3>Welcome!</h3>
+          <img
+            src={require("../images/aylus-logo.png")}
+            class="rounded mx-auto d-block"
+            alt="..."
+          />
+          <Row className="p-4 justify-content-center">
+            <p style={{ lineHeight: "200%" }}>
+              <span style={{ fontWeight: "bold" }}>AYLUS Irvine</span> has an
+              initiative of giving back to the community through service in
+              multifaceted events for the betterment of society. We strive to
+              cultivate a love for volunteering.
+              <br />
+              <br />
+              <span style={{ fontWeight: "bold" }}>
+                This is where you can access our current events (and sign up for
+                them, check your hours, etc.).
+              </span>
+              <br />
+              <br />
+              The projects we do include{" "}
+              <span style={{ color: "#ff0000" }}>h</span>
+              <span style={{ color: "#fc0103" }}>e</span>
+              <span style={{ color: "#fa0205" }}>l</span>
+              <span style={{ color: "#f70308" }}>p</span>
+              <span style={{ color: "#f5040a" }}>i</span>
+              <span style={{ color: "#f2050d" }}>n</span>
+              <span style={{ color: "#ef0610" }}>g</span>
+              <span style={{ color: "#ed0712" }}> </span>
+              <span style={{ color: "#ea0815" }}>o</span>
+              <span style={{ color: "#e80917" }}>u</span>
+              <span style={{ color: "#e5091a" }}>t</span>
+              <span style={{ color: "#e20a1d" }}> </span>
+              <span style={{ color: "#e00b1f" }}>a</span>
+              <span style={{ color: "#dd0c22" }}>t</span>
+              <span style={{ color: "#db0d24" }}> </span>
+              <span style={{ color: "#d80e27" }}>s</span>
+              <span style={{ color: "#d50f2a" }}>e</span>
+              <span style={{ color: "#d3102c" }}>n</span>
+              <span style={{ color: "#d0112f" }}>i</span>
+              <span style={{ color: "#ce1231" }}>o</span>
+              <span style={{ color: "#cb1334" }}>r</span>
+              <span style={{ color: "#c81437" }}> </span>
+              <span style={{ color: "#c61539" }}>c</span>
+              <span style={{ color: "#c3163c" }}>e</span>
+              <span style={{ color: "#c1173e" }}>n</span>
+              <span style={{ color: "#be1841" }}>t</span>
+              <span style={{ color: "#bb1944" }}>e</span>
+              <span style={{ color: "#b91a46" }}>r</span>
+              <span style={{ color: "#b61b49" }}>s</span>
+              <span style={{ color: "#b41c4b" }}>,</span>
+              <span style={{ color: "#b11c4e" }}> </span>
+              <span style={{ color: "#ae1d51" }}>t</span>
+              <span style={{ color: "#ac1e53" }}>e</span>
+              <span style={{ color: "#a91f56" }}>a</span>
+              <span style={{ color: "#a72058" }}>c</span>
+              <span style={{ color: "#a4215b" }}>h</span>
+              <span style={{ color: "#a1225e" }}>i</span>
+              <span style={{ color: "#9f2360" }}>n</span>
+              <span style={{ color: "#9c2463" }}>g</span>
+              <span style={{ color: "#9a2565" }}> </span>
+              <span style={{ color: "#972668" }}>y</span>
+              <span style={{ color: "#94276b" }}>o</span>
+              <span style={{ color: "#92286d" }}>u</span>
+              <span style={{ color: "#8f2970" }}>n</span>
+              <span style={{ color: "#8d2a72" }}>g</span>
+              <span style={{ color: "#8a2b75" }}> </span>
+              <span style={{ color: "#872c78" }}>s</span>
+              <span style={{ color: "#852d7a" }}>t</span>
+              <span style={{ color: "#822e7d" }}>u</span>
+              <span style={{ color: "#7f2e80" }}>d</span>
+              <span style={{ color: "#7d2f82" }}>e</span>
+              <span style={{ color: "#7a3085" }}>n</span>
+              <span style={{ color: "#783187" }}>t</span>
+              <span style={{ color: "#75328a" }}>s</span>
+              <span style={{ color: "#72338d" }}>,</span>
+              <span style={{ color: "#70348f" }}> </span>
+              <span style={{ color: "#6d3592" }}>s</span>
+              <span style={{ color: "#6b3694" }}>e</span>
+              <span style={{ color: "#683797" }}>r</span>
+              <span style={{ color: "#65389a" }}>v</span>
+              <span style={{ color: "#63399c" }}>i</span>
+              <span style={{ color: "#603a9f" }}>n</span>
+              <span style={{ color: "#5e3ba1" }}>g</span>
+              <span style={{ color: "#5b3ca4" }}> </span>
+              <span style={{ color: "#583da7" }}>a</span>
+              <span style={{ color: "#563ea9" }}>t</span>
+              <span style={{ color: "#533fac" }}> </span>
+              <span style={{ color: "#5140ae" }}>l</span>
+              <span style={{ color: "#4e41b1" }}>o</span>
+              <span style={{ color: "#4b41b4" }}>c</span>
+              <span style={{ color: "#4942b6" }}>a</span>
+              <span style={{ color: "#4643b9" }}>l</span>
+              <span style={{ color: "#4444bb" }}> </span>
+              <span style={{ color: "#4145be" }}>f</span>
+              <span style={{ color: "#3e46c1" }}>o</span>
+              <span style={{ color: "#3c47c3" }}>o</span>
+              <span style={{ color: "#3948c6" }}>d</span>
+              <span style={{ color: "#3749c8" }}> </span>
+              <span style={{ color: "#344acb" }}>b</span>
+              <span style={{ color: "#314bce" }}>a</span>
+              <span style={{ color: "#2f4cd0" }}>n</span>
+              <span style={{ color: "#2c4dd3" }}>k</span>
+              <span style={{ color: "#2a4ed5" }}>s</span>
+              <span style={{ color: "#274fd8" }}>,</span>
+              <span style={{ color: "#2450db" }}> </span>
+              <span style={{ color: "#2251dd" }}>a</span>
+              <span style={{ color: "#1f52e0" }}>n</span>
+              <span style={{ color: "#1d53e2" }}>d</span>
+              <span style={{ color: "#1a54e5" }}> </span>
+              <span style={{ color: "#1754e8" }}>m</span>
+              <span style={{ color: "#1555ea" }}>u</span>
+              <span style={{ color: "#1256ed" }}>c</span>
+              <span style={{ color: "#1057ef" }}>h</span>
+              <span style={{ color: "#0d58f2" }}> </span>
+              <span style={{ color: "#0a59f5" }}>m</span>
+              <span style={{ color: "#085af7" }}>o</span>
+              <span style={{ color: "#055bfa" }}>r</span>
+              <span style={{ color: "#035cfc" }}>e</span>
+              <span style={{ color: "#005dff" }}>.</span> Our events are mainly
+              targeted at helping society while also allowing volunteers to gain
+              more team and leadership experience. <br />
+              <br /> We ARE active during COVID-19, and{" "}
+              <span style={{ fontWeight: "bold" }}>everyone is welcome!</span>
+            </p>
+          </Row>
+          <Row className="p-3 justify-content-center">
+            <h5>
+              To get started, <Link to="/register">register.</Link>
+            </h5>
+          </Row>
+          <Row className="p-1 justify-content-center">
+            <h6>
+              For more info, consult our <Link to="/faq">FAQ.</Link>
+            </h6>
+          </Row>
+        </FadeIn>
       </Container>
     );
   }
